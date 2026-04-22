64b587eeb4cc0a352afab1e492d80ac54a8d7e4f
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-04-22 14:59:21 +0200

    feat: add product, category and brand, add collection navigation properties to Brand and Category

--- E-Commerce.Core/Entities/Product.cs
+++ E-Commerce.Core/Entities/Product.cs
@@ -7,5 +7,10 @@ namespace E_Commerce.Core.Entities
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
+
+        public Guid CategoryId { get; set; }
+        public ProductCategory ProductCategory { get; set; } = null!;
+        public Guid ProductId { get; set; }
+        public ProductBrand ProductBrand { get; set; } = null!;
     }
 }
--- E-Commerce.Core/Entities/ProductCategory.cs
+++ E-Commerce.Core/Entities/ProductCategory.cs
@@ -1,7 +1,10 @@
 ﻿namespace E_Commerce.Core.Entities
 {
     public class ProductCategory : BaseEntity
-        {
-            public string Name { get; set; } = string.Empty;
+    {
+        public string Name { get; set; } = string.Empty;
+            
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+
     }
 }
