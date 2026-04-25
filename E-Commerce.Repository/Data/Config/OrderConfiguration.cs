e99aef326996a496b4e69cd9386babf7425e837e
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-04-25 18:39:09 +0200

    add data base context

---
+++ E-Commerce.Repository/Data/Config/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Data.Config
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ E-Commerce.Repository/Data/StoreContext.cs
@@ -0,0 +1,32 @@
+﻿using E_Commerce.Core.Entities.OrderAggregate;
+using E_Commerce.Core.Entities.ProductAggregate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options): base(options) { }  
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // This line defines the EF core to checks for any other settings we have configured.(at reposatoriy project)
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
