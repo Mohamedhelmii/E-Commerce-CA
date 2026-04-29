17ae57622002691dac542363d5e7a72465e5cc50
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-04-29 13:46:16 +0200

    add implementation of IGnericRepo at Repository project

---
+++ E-Commerce.Repository/Repository/GenericRepo.cs
@@ -0,0 +1,29 @@
+﻿using E_Commerce.Core.Entities;
+using E_Commerce.Core.Interface;
+using E_Commerce.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Repository
+{
+    public class GenericRepo<T> : IGenericRepo<T> where T : BaseEntity
+    {
+        private readonly StoreContext sContext;
+        public GenericRepo(StoreContext _storeContext)
+        {
+            sContext = _storeContext;
+        }
+        async Task<T?> IGenericRepo<T>.GetByIdAsync(Guid id)
+        {
+            return await sContext.Set<T>().FindAsync(id);
+        }
+        async Task<IReadOnlyList<T>> IGenericRepo<T>.GetAllAsync()
+        {
+            return await sContext.Set<T>().ToListAsync();
+        }
+    }
+}
