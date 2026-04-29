81042ea4a19ab3a391f5bd71da33801b20c9c523
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-04-29 13:38:43 +0200

    add interface generic repo at core project

---
+++ E-Commerce.Core/Interface/IGenericRepo.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Interface
+{
+    public interface IGenericRepo<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+    }
+}
