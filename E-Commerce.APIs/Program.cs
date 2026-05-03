ac07cc34ef6c636ac3db899d37a6999e938ddc40
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-03 17:21:52 +0200

    fix: resolve Swagger launch issues, project references, and API routing conflicts

--- E-Commerce.APIs/Program.cs
+++ E-Commerce.APIs/Program.cs
@@ -15,8 +15,8 @@ namespace E_Commerce.APIs
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<StoreContext>(
                 option =>
                 {
@@ -28,10 +28,10 @@ namespace E_Commerce.APIs
             builder.Services.AddScoped(typeof(IGenericRepo<>), typeof(GenericRepo<>));
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
