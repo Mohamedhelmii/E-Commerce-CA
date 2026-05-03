70cb18665b03e063f7df83e02c386387a029bb90
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-03 16:59:55 +0200

    feat: add ProductsController before usinf=g specification pattern.

--- E-Commerce.APIs/Controllers/ProductController.cs
+++ E-Commerce.APIs/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using E_Commerce.Core.Entities.ProductAggregate;
+using E_Commerce.Core.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.APIs.Controllers
@@ -7,5 +9,27 @@ namespace E_Commerce.APIs.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IGenericRepo<Product> _ProductRepo;
+
+        public ProductController(IGenericRepo<Product> ProductRepo)
+        {
+            _ProductRepo = ProductRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetProductById(Guid id)
+        {
+            var p = await _ProductRepo.GetByIdAsync(id);
+            if(p == null) return NotFound();
+            return Ok(p);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetAllProducts()
+        {
+            var p = await _ProductRepo.GetAllAsync();
+            return Ok(p);
+        }
+
     }
 }
