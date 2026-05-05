88b71cda7a2fd90600d85803406b2771f38d170f
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-05 13:49:48 +0200

    feat: configure AutoMapper profiles with custom member mapping for Brand and Category names

---
+++ E-Commerce.APIs/Helpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using E_Commerce.Core.DTOs;
+using E_Commerce.Core.Entities.ProductAggregate;
+
+namespace E_Commerce.APIs.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToRetyrnDTO>()
+                .ForMember(PDTO => PDTO.Brand, p => p.MapFrom(pr => pr.Brand.Name));
+
+            CreateMap<Product, ProductToRetyrnDTO>()
+               .ForMember(PDTO => PDTO.Category, p => p.MapFrom(pr => pr.Category.Name));
+        }
+    }
+}
