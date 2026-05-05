81327492784a4d6111f36dd60aa5fbdf0d2a8525
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-05 15:18:41 +0200

    refactor : Brands and Categories controllers to use IMapper and return DTOs

--- E-Commerce.APIs/Controllers/BrandsController.cs
+++ E-Commerce.APIs/Controllers/BrandsController.cs
@@ -1,4 +1,6 @@
-﻿using E_Commerce.Core.Entities.ProductAggregate;
+﻿using AutoMapper;
+using E_Commerce.Core.DTOs;
+using E_Commerce.Core.Entities.ProductAggregate;
 using E_Commerce.Core.Services;
 using E_Commerce.Core.Specification.ProductSpec;
 using Microsoft.AspNetCore.Http;
@@ -11,25 +13,46 @@ namespace E_Commerce.APIs.Controllers
     public class BrandsController : ControllerBase
     {
         private IGenericRepo<ProductBrand> _BrandRepo { get; }
-        public BrandsController(IGenericRepo<ProductBrand> BrandRepo)
+        private readonly IMapper _mapper;
+
+        public BrandsController(IGenericRepo<ProductBrand> BrandRepo, IMapper mapper)
         {
             _BrandRepo = BrandRepo;
+            _mapper = mapper;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ProductBrand>> GetBrandByID(Guid id)
-        { 
+        public async Task<ActionResult<BrandsToReturnDTO>> GetBrandByID(Guid id)
+        {
             var b = await _BrandRepo.GetByIdAsync(id);
             if (b == null) return NotFound();
-            return Ok(b);
+            return Ok(_mapper.Map<ProductBrand, BrandsToReturnDTO>(b));
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetAllBrands()
+        public async Task<ActionResult<IReadOnlyList<BrandsToReturnDTO>>> GetAllBrands()
         {
             var brandSpec = new BrandSpec();
             var b = await _BrandRepo.GetAllWithSpecificationAsync(brandSpec);
-            return Ok(b);
+            return Ok(_mapper.Map<IReadOnlyList<ProductBrand>, IReadOnlyList<BrandsToReturnDTO>>(b));
         }
+
+        #region Old before use mapping
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<ProductBrand>> GetBrandByID(Guid id)
+        //{
+        //    var b = await _BrandRepo.GetByIdAsync(id);
+        //    if (b == null) return NotFound();
+        //    return Ok(b);
+        //}
+
+        //[HttpGet]
+        //public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetAllBrands()
+        //{
+        //    var brandSpec = new BrandSpec();
+        //    var b = await _BrandRepo.GetAllWithSpecificationAsync(brandSpec);
+        //    return Ok(b);
+        //}
+        #endregion
     }
 }
--- E-Commerce.APIs/Controllers/CategoriesController.cs
+++ E-Commerce.APIs/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using E_Commerce.Core.Entities.ProductAggregate;
+﻿using AutoMapper;
+using E_Commerce.Core.DTOs;
+using E_Commerce.Core.Entities.ProductAggregate;
 using E_Commerce.Core.Services;
 using E_Commerce.Core.Specification.ProductSpec;
 using Microsoft.AspNetCore.Http;
@@ -11,25 +13,45 @@ namespace E_Commerce.APIs.Controllers
     public class CategoriesController : ControllerBase
     {
         private IGenericRepo<ProductCategory> _CategoryRepo { get; }
-        public CategoriesController(IGenericRepo<ProductCategory> CategoryRepo)
+        private readonly IMapper _mapper;
+     
+        public CategoriesController(IGenericRepo<ProductCategory> CategoryRepo, IMapper mapper)
         {
             _CategoryRepo = CategoryRepo;
+            _mapper = mapper;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ProductCategory>> GetCategoryByID(Guid id)
+        public async Task<ActionResult<CategoeiesToReturnDTO>> GetCategoryByID(Guid id)
         {
             var c = await _CategoryRepo.GetByIdAsync(id);
             if (c == null) return NotFound();
-            return Ok(c);
+            return Ok(_mapper.Map<ProductCategory, CategoeiesToReturnDTO>(c));
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductCategory>>> GetAllCategories()
+        public async Task<ActionResult<IReadOnlyList<CategoeiesToReturnDTO>>> GetAllCategories()
         {
             var categorySpec = new CategorySpec();
             var c = await _CategoryRepo.GetAllWithSpecificationAsync(categorySpec);
-            return Ok(c);
+            return Ok(_mapper.Map<IReadOnlyList<ProductCategory>, IReadOnlyList<CategoeiesToReturnDTO>>(c));
         }
+        #region old before use mapping
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<ProductCategory>> GetCategoryByID(Guid id)
+        //{
+        //    var c = await _CategoryRepo.GetByIdAsync(id);
+        //    if (c == null) return NotFound();
+        //    return Ok(c);
+        //}
+
+        //[HttpGet]
+        //public async Task<ActionResult<IReadOnlyList<ProductCategory>>> GetAllCategories()
+        //{
+        //    var categorySpec = new CategorySpec();
+        //    var c = await _CategoryRepo.GetAllWithSpecificationAsync(categorySpec);
+        //    return Ok(c);
+        //}
+        #endregion
     }
 }
