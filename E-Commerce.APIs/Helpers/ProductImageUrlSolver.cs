93f861f186592da0a24c68efe5d66a392f005035
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-05 16:53:24 +0200

    feat: implement ProductUrlResolver using IConfiguration

---
+++ E-Commerce.APIs/Helpers/ProductImageUrlSolver.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using E_Commerce.Core.DTOs;
+using E_Commerce.Core.Entities.ProductAggregate;
+
+namespace E_Commerce.APIs.Helpers
+{
+    public class ProductImageUrlSolver : IValueResolver<Product, ProductToRetyrnDTO, string>
+    {
+        private readonly IConfiguration _config;
+        public ProductImageUrlSolver(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string Resolve(Product source, ProductToRetyrnDTO destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.ImageUrl)) return _config["ApiUrl"] + source.ImageUrl;
+
+            return null;
+
+        }
+    }
+}
