83aeaa37a8b954eccc6c7ec0aa3cc9fcad8049b0
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-05 15:06:47 +0200

    feat : add Brands and Categories DTOs.

---
+++ E-Commerce.Core/DTOs/BrandsToReturnDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.DTOs
+{
+    public class BrandsToReturnDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
