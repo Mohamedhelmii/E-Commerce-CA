3453e1d6144b9b054f9dab0b866c4f82f096d758
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-08 19:19:59 +0200

    feat: add BaseApiController and initial BuggyController endpoints implement ApiResponse model

---
+++ E-Commerce.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+﻿using E_Commerce.Repository.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.APIs.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _storeContext;
+        public BuggyController(StoreContext context)
+        {
+            _storeContext = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFound()
+        {
+            var thing = _storeContext.Products.Find(100);
+            return (thing == null) ? NotFound() : Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult ServerError()
+        {
+            var thing = _storeContext.Products.Find(100);
+            var thingTOREturn = thing.ToString();
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetValidationError(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ E-Commerce.APIs/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace E_Commerce.APIs.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public ApiResponse(int status, string message = null)
+        {
+            StatusCode = status;
+            ErrorMessage = message ?? GetDefaultMessageForStatusCode(status);
+        }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request, you have made",
+                401 => "Authorized, you are not",
+                404 => "Resource found, it was not",
+                500 => "Errors are the path to the dark side",
+                _ => null
+            };
+        }
+    }
+}
