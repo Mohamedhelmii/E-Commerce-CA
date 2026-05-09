57a9e930826d6cc3ab0fce61d4f4a8f48de52b70
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-09 14:19:43 +0200

    feat: implement Global Exception Middleware and ApiException model

---
+++ E-Commerce.APIs/Errors/ApiException.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace E_Commerce.APIs.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        private string Details { get; set; }
+        public ApiException(int statusCode, string message = null, string details = null)
+        : base(statusCode, message)
+        {
+            Details = details;  
+        }
+    }
+    #region Description
+        // Added ApiException to extend ApiResponse with error details(StackTrace).
+        // Created ExceptionMiddleware to catch all unhandled exceptions globally.
+        // Configured the middleware to return consistent JSON responses based on the environment (Dev vs Prod).
+    #endregion
+}
---
+++ E-Commerce.APIs/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace E_Commerce.APIs.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionMiddleware> logger, IHostEnvironment hostEnvironment)
+        {
+            _next = requestDelegate;
+            _logger = logger;
+            _env = hostEnvironment;
+        }
+
+        public async void Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);//Record the mistake in the console
+                context.Response.ContentType = "application/json";//telling the browser, i send json
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+                    : new ApiException((int)HttpStatusCode.InternalServerError);
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
