32f7a6daabff9e7bf7d8bf8318ea6b149456d167
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-13 15:08:04 +0200

    feat: add ProductWithFiltersForCountSpecification

---
+++ E-Commerce.Core/Specification/ProductSpec/ProductWithFilterCountSpec.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Core.Entities.ProductAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Specification.ProductSpec
+{
+    public class ProductWithFilterCountSpec : BaseSpecification<Product>
+    {
+        public ProductWithFilterCountSpec(ProductSpecParams productSpecParams) 
+            : base( p =>
+            (!productSpecParams.BrandIdFilter.HasValue || p.BrandId == productSpecParams.BrandIdFilter) &&
+            (!productSpecParams.CategoryIdFilter.HasValue || p.CategoryId == productSpecParams.CategoryIdFilter) &&
+            (string.IsNullOrEmpty(productSpecParams.Search) || p.Name.ToLower().Contains(productSpecParams.Search))
+            )
+        {
+        }
+    }
+}
