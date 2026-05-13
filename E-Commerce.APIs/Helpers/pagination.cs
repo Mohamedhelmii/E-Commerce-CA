dea12833d093d6d24c3cfd64430f291f3e404272
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-13 15:01:10 +0200

    feat: add Pagination helper class for API responses

---
+++ E-Commerce.APIs/Helpers/pagination.cs
@@ -0,0 +1,17 @@
+﻿namespace E_Commerce.APIs.Helpers
+{
+    public class pagination<T> where T : class
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int Count { get; set; }
+        public IReadOnlyList<T> Data { get; set; }
+        public pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            Count = count;
+            Data = data;
+        }
+    }
+}
