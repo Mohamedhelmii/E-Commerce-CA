777a5cfbc7b8069e8ceae54dd8eeab40851eeeb8
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-18 13:41:46 +0200

    feat: add CustomerBasket and BasketItem entities.

---
+++ E-Commerce.Core/Entities/Basket/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Commerce.Core.Entities.Basket
+{
+    public class BasketItem
+    {
+        public Guid productId { get; set; }
+        public string productName { get; set; } = string.Empty;
+        public string imageUrl { get; set; } = string.Empty;
+        public decimal price { get; set; }
+        public int quantity { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
---
+++ E-Commerce.Core/Entities/Basket/CustomarBasket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Entities.Basket
+{
+    public class CustomarBasket
+    {
+        // to use when deserialization
+        public CustomarBasket() { }
+
+        //to create basket if it didn't in one line
+        public CustomarBasket(string id)
+        {
+            customarId = id;
+        }
+        public string customarId { get; set; } = string.Empty;
+        public List<BasketItem> basketItems { get; set; } = new List<BasketItem>();
+    }
+}
