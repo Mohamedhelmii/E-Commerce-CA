05d253ed4a6d2833482481165332ede034e8ec92
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-18 13:58:48 +0200

    feat: add IBasketRepository interface in Core layer

---
+++ E-Commerce.Core/Services/IBasketRepo.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce.Core.Entities.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Services
+{
+    public interface IBasketRepo
+    {
+        Task<CustomarBasket?> GetBasketAsync(string basketId);
+        Task<CustomarBasket?> AddorUpdateBasketAsync(CustomarBasket Basket);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
