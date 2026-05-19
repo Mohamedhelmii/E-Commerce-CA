4eeb38d39bbfc3d642c81732b54fa2665257e0b8
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-19 16:01:08 +0200

    Db:  add-ProductItemOrderd and update Relations

--- E-Commerce.Repository/Data/Migrations/20260519135443_add-ProductItemOrderd_and_updateRelations.Designer.cs
+++ E-Commerce.Repository/Data/Migrations/20260519135443_add-ProductItemOrderd_and_updateRelations.Designer.cs
@@ -4,6 +4,7 @@ using E_Commerce.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_Commerce.Repository.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260519135443_add-ProductItemOrderd_and_updateRelations")]
+    partial class addProductItemOrderd_and_updateRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ E-Commerce.Repository/Data/Migrations/20260519135443_add-ProductItemOrderd_and_updateRelations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Commerce.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductItemOrderd_and_updateRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "OrderItems",
+                newName: "ProductItemOrdered_ProductName");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductItemId",
+                table: "OrderItems",
+                newName: "ProductItemOrdered_ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "OrderItems",
+                newName: "ProductItemOrdered_ImageUrl");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OrderId",
+                table: "OrderItems",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductItemOrdered_ProductName",
+                table: "OrderItems",
+                newName: "ProductName");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductItemOrdered_ProductId",
+                table: "OrderItems",
+                newName: "ProductItemId");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductItemOrdered_ImageUrl",
+                table: "OrderItems",
+                newName: "PictureUrl");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OrderId",
+                table: "OrderItems",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
