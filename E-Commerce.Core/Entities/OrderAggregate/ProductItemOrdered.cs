3bd99c7683db543225d1bd22ddd69e7600c6858c
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-19 14:13:41 +0200

    Feat: Add ProductItemOrdered value object

---
+++ E-Commerce.Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Entities.OrderAggregate
+{
+    public class ProductItemOrdered
+    {
+        public ProductItemOrdered() {}
+        public ProductItemOrdered(Guid productId, string productName, string imageUrl)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            ImageUrl = imageUrl;
+        }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
