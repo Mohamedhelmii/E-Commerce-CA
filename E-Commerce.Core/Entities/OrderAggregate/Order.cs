832b479a82133879c594309f578f3b6162807322
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-19 14:33:41 +0200

    Feat: - Changed OrderItems to IReadOnlyList to prevent external modification. - Added empty and parameterful constructors for EF Core and business logic safety.

--- E-Commerce.Core/Entities/OrderAggregate/Order.cs
+++ E-Commerce.Core/Entities/OrderAggregate/Order.cs
@@ -2,13 +2,23 @@
 {
     public class Order : BaseEntity
     {
+        public Order() { }
+        public Order(string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod,
+                     IReadOnlyList<OrderItem> orderItems, decimal subtotal)
+        {
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            Subtotal = subtotal;
+        }
         public string BuyerEmail { get; set; } = string.Empty;
         public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
         // The shipping address for the order, which includes details such as street, city, state, postal code, and country, used for delivering the order to the customer.
         public Address ShipToAddress { get; set; } = new Address();
         public Guid DeliveryMethodId { get; set; }
         public DeliveryMethod DeliveryMethod { get; set; } = new DeliveryMethod();
-        public ICollection<OrderItem> OrderItems { get; set; } = new HashSet<OrderItem>();
+        public IReadOnlyList<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
         public decimal Subtotal { get; set; }
         // The status of the order, which can be Pending, PaymentReceived, Shipped, etc., used to track the order's progress through the fulfillment process.
         public OrderStatus Status { get; set; } = OrderStatus.Pending;
