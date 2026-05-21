20110878f244c0209b7c20f68d4a07c1316802a3
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-21 15:05:17 +0200

    Feat: Implement OrderWithItemsSpecification with dual constructors

---
+++ E-Commerce.Core/Specification/OrderSpec/OrderWithItemSpec.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce.Core.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Specification.OrderSpec
+{
+    public class OrderWithItemSpec : BaseSpecification<Order>
+    {
+        //to return history of orders
+        public OrderWithItemSpec(string buyerEmail) : base(o => o.BuyerEmail == buyerEmail) 
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+            AddOrderByDescending(o => o.OrderDate);
+        }
+
+        //to return details of specific order
+        public OrderWithItemSpec(Guid orderId, string buyerEmail)
+            : base(o => o.Id == orderId && o.BuyerEmail == buyerEmail)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude(o => o.DeliveryMethod);
+        }
+    }
+}
