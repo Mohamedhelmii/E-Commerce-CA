9793d54afe122f31bf0fca50f5d26f51123cad72
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-05-21 14:47:14 +0200

    Feat: Define Core interfaces for Order Service and Unit of Work

---
+++ E-Commerce.Core/Services/IOrderServices.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Core.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Services
+{
+    public interface IOrderServices
+    {
+        public Task<Order?> CreateOrderAsync(string buyerEmail, string basketId, Guid deliveryMethodId, Address address);
+        public Task<Order?> GetOrderForUserAsync(string buyerEmail);
+        public Task<Order?> GetOrderByIdForUserAsync(Guid orderId, string buyerEmail);
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+    }
+}
---
+++ E-Commerce.Core/Services/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Services
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        public IGenericRepo<TEntity> GenericRepo<TEntity>() where TEntity : BaseEntity;
+        Task<int> SaveAsync();
+    }
+}
