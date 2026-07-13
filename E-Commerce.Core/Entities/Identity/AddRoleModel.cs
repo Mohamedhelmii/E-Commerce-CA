62222a8d3741b3e87642cb66e65858ff1227d998
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-13 22:20:49 +0200

    add models to refrsh token and auth model

---
+++ E-Commerce.Core/Entities/Identity/AddRoleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Entities.Identity
+{
+    public class AddRoleModel
+    {
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string Role { get; set; }
+    }
+}
---
+++ E-Commerce.Core/Entities/Identity/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Entities.Identity
+{
+    [Owned]
+    public class RefreshToken
+    {
+        public string Token { get; set; }
+        public DateTime ExpiersOn { get; set; }
+        public bool IsExpierd => DateTime.UtcNow > ExpiersOn;
+        public DateTime CreatedOn { get; set; }
+        public DateTime? RevokedOn { get; set; }
+        public bool IsActive => !IsExpierd && RevokedOn == null;
+    }
+}
