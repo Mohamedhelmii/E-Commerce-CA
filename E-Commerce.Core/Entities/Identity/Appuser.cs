ae0a5c8d0d8c37d43bb81359c0d7d7415ecb3d93
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-13 22:13:13 +0200

    add appUser Model

---
+++ E-Commerce.Core/Entities/Identity/Appuser.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Core.Entities.OrderAggregate;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Entities.Identity
+{
+    public class Appuser : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public Adress adress { get; set; }
+
+        public List<RefreshToken>? RefreshTokens { get; set; }
+
+        public DateTime? RefreshTokenExpiration { get; set; }
+
+    }
+}
