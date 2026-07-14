3fc3af7760430fe93fe4d3f615d74ec07251e9c7
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-14 15:06:48 +0200

    add identity context

---
+++ E-Commerce.Repository/Data/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Data.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<Appuser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
