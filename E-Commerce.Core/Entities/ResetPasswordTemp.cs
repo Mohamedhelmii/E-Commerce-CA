f75eb1d4ba46f93c2359ff570f3a1ca4d13511a4
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-14 15:04:10 +0200

    add models to control password

---
+++ E-Commerce.Core/Entities/ResetPasswordTemp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Entities
+{
+    public class ResetPasswordTemp
+    {
+        [Key]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string OtpCode { get; set; }
+
+        [Required]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
