61e10b167cc116633e5fc1233145a222e1d5fb20
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-15 16:18:18 +0200

    add Auth services

---
+++ E-Commerce.Core/Interfaces/IAuthService.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Core.DTOs.Identity;
+using E_Commerce.Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> InitSignUpAsync(RegisterDto model);
+        Task<AuthModel> LoginAsync(LoginDto model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+        Task<AuthModel> RefreshTokenAsync(string token);
+        Task<bool> RevokeTokenAsync(string token);
+        Task<AuthModel> VerifyCodeAsync(string email, string inputCode);
+        Task<SignUpResponseDto> ReSendCode(string email);
+        Task<SendOtpCodeResponceDto> SendOtpCode(SendOtpCodeRequestDto model);
+        Task<VerifyOtpResponseDto> VerifyResetPasswordOtpAsync(VerifyOtpRequestDto model);
+        Task<ResetPasswordResponseDto> ResetPasswordAsync(ResetPasswordRequestDto model);
+        Task<bool> LogoutAsync(string refreshToken);
+    }
+}
