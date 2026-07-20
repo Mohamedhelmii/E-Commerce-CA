98f29b4faaf2db7b760ee1ff5e88c1a0bf47bcd8
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-20 14:23:18 +0200

    feat: add ITemporaryUserRepository interface

---
+++ E-Commerce.Core/Interfaces/ITemporaryUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.Core.Entities;
+
+namespace E_Commerce.Core.Interfaces
+{
+    public interface ITemporaryUserRepository
+    {
+        Task<UserStoreTemporary> GetByEmailAsync(string email);
+        Task AddAsync(UserStoreTemporary user);
+        Task UpdateAsync(UserStoreTemporary user);
+        Task DeleteAsync(UserStoreTemporary user);
+    }
+}
