17a77bbb200ab67d804f8b79d5c123f349b736c5
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-20 14:21:49 +0200

    feat: add ITokenService interface

---
+++ E-Commerce.Core/Interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using E_Commerce.Core.Entities.Identity;
+
+namespace E_Commerce.Core
+{
+    public interface ITokenService
+    {
+
+        Task<string> CreateTokenAsync(Appuser user, UserManager<Appuser> userManager);
+    }
+}
