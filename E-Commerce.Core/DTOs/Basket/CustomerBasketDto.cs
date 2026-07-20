2672900c9684ad3a8091dedcb102463584f2c0b8
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-20 14:21:28 +0200

    feat: add basket item and customer basket DTOs

---
+++ E-Commerce.Core/DTOs/Basket/CustomerBasketDto.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Core.DTOs.Basket
+{
+    public class CustomerBasketDto
+    {
+        public string Id { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public string? ClientSecret { get; set; }
+        public int? DeliveryMethod { get; set; }
+    }
+}
