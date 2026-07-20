5078d6c8fb61fc45081ab26d2f5ff790a2afa238
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-20 14:22:14 +0200

    feat: add IResponseCacheService interface

---
+++ E-Commerce.Core/Interfaces/IResponcecasheservie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.Interfaces
+{
+    public interface IResponcecasheservie
+    {
+        //cache data
+        Task CasheResponceAsync(string Cachekey, object Responce, TimeSpan ExpireTime);
+
+
+        //get data from cache
+        Task<string?> Getcacheresponce(string Cachekey);
+    }
+}
