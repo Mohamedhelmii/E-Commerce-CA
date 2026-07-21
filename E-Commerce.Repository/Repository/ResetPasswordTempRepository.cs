3888126fbbe80a5e86feaebcabacedc33c502495
Mohamed Helmy <mohamedhelmy0209@gmail.com>
2026-07-21 18:55:57 +0200

    feat: implement temporary user and reset password repositories

---
+++ E-Commerce.Repository/Repository/ResetPasswordTempRepository.cs
@@ -0,0 +1,48 @@
+﻿using E_Commerce.Core.Entities;
+using E_Commerce.Core.Interfaces;
+using E_Commerce.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Repository
+{
+    public class ResetPasswordTempRepository : IResetPasswordTempRepository
+    {
+        private readonly StoreContext _context;
+
+        public ResetPasswordTempRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResetPasswordTemp?> GetByEmailAsync(string email)
+        {
+            return await _context.ResetPasswordTemps.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task AddAsync(ResetPasswordTemp entity)
+        {
+            await _context.ResetPasswordTemps.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(ResetPasswordTemp entity)
+        {
+            _context.ResetPasswordTemps.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ResetPasswordTemp entity)
+        {
+            _context.ResetPasswordTemps.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+
+
+    }
+}
---
+++ E-Commerce.Repository/Repository/TemporaryUserRepository.cs
@@ -0,0 +1,44 @@
+﻿using E_Commerce.Core.Entities;
+using E_Commerce.Core.Interfaces;
+using E_Commerce.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository.Repository
+{
+    public class TemporaryUserRepository : ITemporaryUserRepository
+    {
+        private readonly StoreContext _context;
+
+        public TemporaryUserRepository(StoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<UserStoreTemporary> GetByEmailAsync(string email)
+        {
+            return await _context.UserStoreTemporary.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task AddAsync(UserStoreTemporary user)
+        {
+            await _context.UserStoreTemporary.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(UserStoreTemporary user)
+        {
+            _context.UserStoreTemporary.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(UserStoreTemporary user)
+        {
+            _context.UserStoreTemporary.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
